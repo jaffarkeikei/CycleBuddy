c321d9a1a912df740489803222f461e0f85d4938
jaffar <keikei.jaffar@mail.utoronto.ca>
2025-05-14 13:52:52 -0400

    Initial demo for the webapp

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+const { defineConfig } = require('vite');
+const react = require('@vitejs/plugin-react');
+const path = require('path');
 
-// https://vitejs.dev/config/
-export default defineConfig({
+module.exports = defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
